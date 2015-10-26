26f0aff294ef9c8d570e6ba4fd9ae51a0d76f551
Vadim L. Avramishin <vavramishin@brightgrove.com>
2015-10-26 14:24:20 +0200

    fix

--- config.php
+++ config.php
@@ -1,1 +1,29 @@
-<?php$cfg = array(    'db' => array(        'lingopanda' => array(            'host' => 'localhost',            'user' => 'lingopanda',            'pass' => '',            'name' => 'lingopanda'        ),    ),    'host' => 'api-lingopanda.makers.do',        'memcache' => array(        'active' => false,        'timeout' => 20,        'host' => 'localhost',        'port' => 11211    ),    'encryptionKey' => '',    'debug' => false,    'publicDir' => sprintf('%s/public', __DIR__),    'logsDir' => sprintf('%s/logs', __DIR__),    'tmpDir' => sprintf('%s/tmp', __DIR__),    'timezone' => 'Europe/Berlin');
+<?php
+$cfg = array(
+
+    'db' => array(
+        'lingopanda' => array(
+            'host' => 'localhost',
+            'user' => 'lingopanda',
+            'pass' => '',
+            'name' => 'lingopanda'
+        ),
+    ),
+
+    'host' => 'api-lingopanda.makers.do',
+    
+    'memcache' => array(
+        'active' => false,
+        'timeout' => 20,
+        'host' => 'localhost',
+        'port' => 11211
+    ),
+
+    'encryptionKey' => '',
+
+    'debug' => false,
+    'publicDir' => sprintf('%s/public', __DIR__),
+    'logsDir' => sprintf('%s/logs', __DIR__),
+    'tmpDir' => sprintf('%s/tmp', __DIR__),
+    'timezone' => 'Europe/Berlin'
+);
